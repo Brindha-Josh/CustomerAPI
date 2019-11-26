5e5ad12d0c51e6aae9f500507d2a1faad6a90a50
Brindha-Josh <brindhamaniam@gmail.com>
2019-11-26 11:34:24 +0100

    adding files to repo

---
+++ Models/PersonDBContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CUSTOMERAPISQL.Models
+{
+    public partial class PersonDBContext : DbContext
+    {
+        public PersonDBContext()
+        {
+        }
+
+        public PersonDBContext(DbContextOptions<PersonDBContext> options)
+            : base(options)
+        {
+        }
+
+
+        public virtual DbSet<Customers> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=PersonDB");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customers>(entity =>
+            {
+                entity.ToTable("CUSTOMERS");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("ID")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Address)
+                    .HasColumnName("ADDRESS")
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Age).HasColumnName("AGE");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("NAME")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
